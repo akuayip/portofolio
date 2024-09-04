927c122e6625a89b1072891e3af989f41c44bea9
akuayip <muhammadarief1234516@gmail.com>
2024-09-04 16:05:23 +0700

    add: link media sosial

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 import {
   IconBrandGithub,
   IconBrandLinkedin,
@@ -7,21 +6,24 @@ import {
 } from "@tabler/icons-react";
 
 const sosmed = [
-  { icon: <IconBrandGithub />, path: "" },
-  { icon: <IconBrandLinkedin />, path: "" },
-  { icon: <IconBrandInstagram />, path: "" },
+  { icon: <IconBrandGithub />, path: "https://github.com/akuayip" },
+  { icon: <IconBrandLinkedin />, path: "https://www.linkedin.com/in/arriefrahman/" },
+  { icon: <IconBrandInstagram />, path: "https://www.instagram.com/arriefrahman_/" },
 ];
 
-const Social = ({ containerStyles, iconStyles }) => {
+type SocialProps = {
+  containerStyles: string;
+  iconStyles: string;
+};
+
+const Social: React.FC<SocialProps> = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
-      {sosmed.map((item, index) => {
-        return (
-          <Link key={index} href={item.path} className={iconStyles}>
-            {item.icon}
-          </Link>
-        );
-      })}
+      {sosmed.map((item, index) => (
+        <Link key={index} href={item.path} className={iconStyles}>
+          {item.icon}
+        </Link>
+      ))}
     </div>
   );
 };
